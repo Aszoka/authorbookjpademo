4c59cdfb6218a888d1ca9239b370c10c41c205a2
Alexandra <hajdu.alexandra09@gmail.com>
2022-01-17 13:13:56 +0100

    added jpa entities + h2 config

--- src/main/java/com/example/demo/model/Author.java
+++ src/main/java/com/example/demo/model/Author.java
@@ -1,4 +1,33 @@
 package com.example.demo.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
 }
--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,4 +1,33 @@
 package com.example.demo.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(name= "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name= "author_id"))
+    private Set<Author> authors;
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
 }
